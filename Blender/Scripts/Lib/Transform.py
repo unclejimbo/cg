16595f059e1ca9a2740875941ac0fa58d1c25189
Zhaoc917 <841781063@qq.com>
2019-12-16 19:31:43 +0800

    Add Transform.py
    Former-commit-id: 3dc5a559e36c144f625cf01c155685b34229b763 [formerly 988469b2fe1b51a3103b23525e58a78d901f5484]
    Former-commit-id: 8bdfee423224526bb7ae52fddf747a29bad9df50

---
+++ Blender/Scripts/Lib/Transform.py
@@ -0,0 +1,23 @@
+import os
+import json
+import bpy
+
+
+if __name__ == "__main__":
+    parent_obj = bpy.data.objects["Parent Object"]
+    path = os.getcwd()
+    path = os.path.dirname(path)
+    path = os.path.dirname(path)
+
+    transform_path = path + "/Data/Transform/transform.json"
+    if not os.path.exists(transform_path):
+        os.system(r"touch {}".format(transform_path))
+    print(transform_path)
+
+    transform_dict = {}
+    transform_dict['Location'] = (parent_obj.location[0], parent_obj.location[1], parent_obj.location[2])
+    transform_dict['Rotation'] = (parent_obj.rotation_euler.x, parent_obj.rotation_euler.y, parent_obj.rotation_euler.z)
+    transform_dict['Scale'] = (parent_obj.scale[0], parent_obj.scale[1], parent_obj.scale[2])
+
+    with open(transform_path, 'w') as dump_f:
+        json.dump(transform_dict, dump_f)
